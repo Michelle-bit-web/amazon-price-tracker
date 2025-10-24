a80bde62d7bb7a6f56f298d0671fadae255c14dd
Michelle <mpuschkarow@web.de>
2025-10-24 13:51:38 +0200

    web scraping used to get price as number

--- amazone-price-tracker/main.py
+++ amazone-price-tracker/main.py
@@ -1,2 +1,20 @@
 import requests
+from bs4 import BeautifulSoup
 
+#Fetch raw HTML content
+url = "https://appbrewery.github.io/instant_pot/"
+
+response = requests.get(url)
+
+#Parse using BeautifulSoup
+bs = BeautifulSoup(response.content, 'html.parser')
+# print(bs.prettify())
+price = bs.find('span', class_='aok-offscreen').get_text()
+
+#Remove $ Sign
+price_number = price.split('$')[1]
+# print(price_number)
+
+#Convert to floating point number
+price_as_float = float(price_number)
+print(price_as_float)
